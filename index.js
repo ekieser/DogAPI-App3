5adfd5da40646a01893d6d34c8e5a0a297d27afe
Ethan Kieser <53837375+ekieser@users.noreply.github.com>
2019-12-02 14:18:58 -0600

    intial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+function retrieveImages() {
+    const userValue = $('#breed-entry').val().toLowerCase();
+    $('#breed-entry').val('');
+        fetch(`https://dog.ceo/api/breed/${userValue}/images/random`)
+        .then(response => response.json())
+        .then(responseJson => logDogs(responseJson))
+        .catch(error => alert('An error occured. Try again later'));
+}
+
+function logDogs(responseJson) {
+    console.log(responseJson);
+    if (responseJson.status !== "success") {
+        alert('Could not find this breed. Try Again');
+    } else if (responseJson === "success") {
+        $('#dog-display').append(`<img src="${responseJson.message}" class="img-results">`);
+    }
+} 
+
+function submitForm() {
+    $('form').submit(event => {
+        event.preventDefault(); 
+        let dogBreed = document.getElementById('breed-entry').value;
+        retrieveImages(dogBreed);
+    });
+}
+
+$(submitForm);
+
